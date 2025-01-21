bf5329cda9585d763faf7432a8689804cd4c7054
engMaya22 <eng.maya.esmaeel1@gmail.com>
2025-01-21 15:38:02 +0300

    user auth

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\AuthAdmin;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -10,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home.index');
+Route::middleware(['auth'])->group(function(){
+    Route::get('/account-dashboard', [UserController::class, 'index'])->name('user.index');
+
+
+});
+Route::middleware(['auth',AuthAdmin::class])->group(function(){
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+
+
+});
 
 // from template take header to replace in app b blade by it ,
 //take body index from template to add to app blade
